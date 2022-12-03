b35787fab59d9ebc279956368f77543d8092cba2
squadoCoding <106444206+squadoCoding@users.noreply.github.com>
2022-12-04 04:58:51 +0530

    learned variables and datatypes in python

---
+++ commentsExcapeSequenceCharactersAndPrintStatement.py
@@ -0,0 +1,22 @@
+print("Hellow, I am a good boy.")
+# This is a single line comment
+# Escape sequence characters in python
+'''We can
+write multiline
+comments in
+python'''
+"""
+And we can either use triple
+single quots or triple double quotes
+to write multiline comments
+in python
+"""
+print("This visitor is also \na good boy") # New line escape sequence character
+print("This visitor is also \"a good boy\"")  # Double quote escape sequence character # New line escape sequence character
+print('This visitor is also \'a good boy\'')  # Single quote escape sequence character # New line escape sequence character
+
+# Print statement
+print("hey",56,8)
+print("hey",56,8,sep="-");
+print("hey",56,89,sep="-",end=" 900\n");
+print("this is my second print statement");
---
+++ variablesAndDatatypes.py
@@ -0,0 +1,27 @@
+# This is tutorial on variables and datatypes
+a = 1335432124
+print(a)
+b = "Siddhant"
+print(b)
+c = True
+print(c)
+d = None
+print(d)
+print(type(d))
+
+# Bulit in datatypes in python
+    # 1.Number
+        # (a) Intiger
+        # (b) Floating point
+        # (c) Complex number
+    # 2.String
+        # Ex: "Python programming","Heavy coder",etc.
+    # 3.Boolean
+        # Can be either true or false
+    # 4.Sequenced data: list,tuple
+        # List: List is a collection of different data elements which can be of different types
+        # tuple: Tuple is exactly like list but it is immutable
+    # 5.Mapped data: dict
+        # These are key:value pairs
+    
+# Note: Every thing is an boject in python
